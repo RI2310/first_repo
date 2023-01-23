b3f1a980f29af11120e259b83f19c982d759afd3
RI2310 <123071605+RI2310@users.noreply.github.com>
2023-01-23 14:40:51 +0530

    Update firstFile.java

--- firstFile.java
+++ firstFile.java
@@ -1,15 +1,26 @@
-Class firstFile
+class firstFile
 {
 int i=2;
-double d=0.0;
+double d,f=0.0;
 public void modulus(int j)
 {
 d=j%2;
 System.out.println("The modulus fo given number is :" +d);
 }
+public void divison(int a,int b)
+{
+	try {
+f= a/b;
+System.out.println("The divison value is :" +f);
+	}catch(Exception e) {
+		System.out.println(e);
+	}
+
+}
 public static void main(String args[])
 {
   firstFile f1= new firstFile();
-  f1.modulus(8);
+  f1.modulus(11);
+  f1.divison(5,0);
 }
 }
